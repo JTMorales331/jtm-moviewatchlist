462a396bb61b1d8aec47261d5f2513efc8e99bcf
Ter[r]y <johnterence.morales@benilde.edu.ph>
2024-09-14 13:28:46 +0800

    added basic header and search function. Can store movies in movieSearchArray upon searching for a movieName

--- index.js
+++ index.js
@@ -1,6 +1,26 @@
-const movieName = 'Star Wars'
-const type = "movie"
 
-const res = await fetch(`http://www.omdbapi.com/?i=tt3896198&apikey=7dbbf5ff&s=${movieName}&type=${type}`)
-const data = await res.json()
-console.log(data)
+let movieName = document.getElementById('search-movie')
+
+let movieSearchArray = []
+
+// buttons based on dataset and id
+document.addEventListener('click', (e) => {
+  if (e.target.id === 'search-btn') {
+    getMovies(movieName.value)
+  }
+})
+
+async function getMovies(movieName) {
+  console.log(movieName)
+  const res = await fetch(`http://www.omdbapi.com/?i=tt3896198&apikey=7dbbf5ff&s=${movieName}`)
+  const data = await res.json()
+  console.log(data)
+  movieSearchArray = data.Search
+
+  console.log(movieSearchArray)
+}
+
+
+function render() {
+
+}
