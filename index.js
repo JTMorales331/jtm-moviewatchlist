de16767ba75d5ceaff924ab14d66ea683d8d829c
Ter[r]y <johnterence.morales@benilde.edu.ph>
2024-09-16 13:22:22 +0800

    watchlistArray gets saved to local storage. Can now render watchlistArray in watchlist.html

--- index.js
+++ index.js
@@ -3,25 +3,96 @@ let movieName = document.getElementById('search-movie')
 
 let movieSearchArray = []
 
+let watchlistArray = []
+
 // buttons based on dataset and id
 document.addEventListener('click', (e) => {
   if (e.target.id === 'search-btn') {
     searchMovie(movieName.value)
   }
+  if (e.target.dataset.imdbid) {
+    addToWatchlist(e.target.dataset.imdbid)
+  }
 })
 
+document.addEventListener('DOMContentLoaded', () => {
+
+  const savedWatchlist = JSON.parse(localStorage.getItem('watchlist'))
+
+  if(savedWatchlist) {
+    watchlistArray = savedWatchlist
+  }
+
+  if(document.getElementById('watchlist-container')) {
+    console.log('watchlist-container rendered')
+    renderWatchlist()
+  }
+})
+
+function addToWatchlist(imdbID) {
+  fetch(`http://www.omdbapi.com/?i=${imdbID}&apikey=7dbbf5ff&plot=short`)
+  .then(res => res.json())
+  .then(movie => {
+    console.log(movie)
+    watchlistArray.push(movie)
+
+    localStorage.setItem('watchlist', JSON.stringify(watchlistArray))
+  })
+}
+
+// function removeFromWatchlist(imdbID) {
+//   fetch(`http://www.omdbapi.com/?i=${imdbID}&apikey=7dbbf5ff&plot=short`)
+//   .then(res => res.json())
+//   .then(movie => {
+//     console.log(movie)
+//     watchlistArray.push(movie)
+//     console.log(watchlistArray)
+//   })
+// }
+
 function searchMovie(movieName) {
-  console.log(movieName)
   fetch(`http://www.omdbapi.com/?apikey=7dbbf5ff&s=${movieName}&plot=short`)
   .then(res => res.json())
   .then(data => {
     movieSearchArray = data.Search
-    getMoviesDetails().then(movieHtml => {
-      render(movieHtml)
-    })
+    render()
   })
-  
-  
+}
+
+function getWatchlistDetails() {
+  console.log(watchlistArray)
+  console.log('getting watchlist')
+  let promises = watchlistArray.map(movie => {
+    return fetch(`http://www.omdbapi.com/?i=${movie.imdbID}&apikey=7dbbf5ff&plot=short`)
+      .then(res => res.json())
+      .then(data => {
+        return `
+          <div class="movie-card">
+            <img class="movie-poster" src="${data.Poster}" />
+            <div class="movie-details">
+              <h1 class="details-one">${data.Title}<span class="imdb-rating"><i class="fa-solid fa-star"></i>${data.imdbRating}</span></h1>
+              
+              <div class="details-two">
+                <p>${data.Runtime}</p>
+                <p>${data.Genre}</p>
+                <button
+                  class="remove-watchlist-btn"
+                  data-watchlistid="${data.imdbID}"
+                >
+                <i class="fa-solid fa-circle-minus"></i>
+                Remove
+                </button>
+              </div>
+
+              <p class="movie-plot">${data.Plot}</p>
+            </div>
+          </div>
+        `
+      })
+  })
+  return Promise.all(promises).then(results => {
+    return results.join('')
+  })
 }
 
 function getMoviesDetails() {
@@ -29,7 +100,6 @@ function getMoviesDetails() {
     return fetch(`http://www.omdbapi.com/?i=${movie.imdbID}&apikey=7dbbf5ff&plot=short`)
       .then(res => res.json())
       .then(data => {
-        console.log(data)
         return `
           <div class="movie-card">
             <img class="movie-poster" src="${movie.Poster}" />
@@ -41,7 +111,7 @@ function getMoviesDetails() {
                 <p>${data.Genre}</p>
                 <button
                   class="add-watchlist-btn"
-                  data-id="${data.imdbID}"
+                  data-imdbid="${data.imdbID}"
                 >
                 <i class="fa-solid fa-circle-plus"></i>
                 Watchlist
@@ -59,6 +129,17 @@ function getMoviesDetails() {
   })
 }
 
-function render(renderHtml) {
-  document.getElementById('search-results').innerHTML = renderHtml
+function render() {
+  getMoviesDetails().then(movieHtml => {
+    document.getElementById('search-results').innerHTML = movieHtml
+  })
+  
+}
+
+function renderWatchlist() {
+  console.log('rendering watchlist')
+  getWatchlistDetails().then(movieHtml => {
+    document.getElementById('watchlist-container').innerHTML = movieHtml
+    console.log('rendering successful')
+  })
 }
